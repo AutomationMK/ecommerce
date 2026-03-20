cb4466e0cdd7079b39d51e42389a49592f7f47d0
Max Kranker <maxkranker@protonmail.com>
2026-03-20 17:43:37 -0400

    Add initial json structs for api handlers

---
+++ cmd/api/handlers-api.go
@@ -0,0 +1,13 @@
+package main
+
+type stripePayload struct {
+	Currency string `json:"currency"`
+	Amount   string `json:"amount"`
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+	Content string `json:"content"`
+	ID      int    `json:"id"`
+}
