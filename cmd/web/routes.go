c0f14fbd3bc2ed4ce6c908abf4797d850083cbcc
Max Kranker <maxkranker@protonmail.com>
2026-03-22 13:01:15 -0400

    Add /payment-succeeded post route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,6 +10,7 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/payment-succeded", app.PaymentSucceeded)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
