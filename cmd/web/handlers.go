532ac255f8aa58766db5987bc4ed5c4cbc42cae9
Max Kranker <maxkranker@protonmail.com>
2026-03-23 16:26:33 -0400

    Add custom test widget to pass as template data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/AutomationMK/ecommerce/internal/models"
+)
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
@@ -50,8 +54,20 @@ func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["publishable_key"] = app.config.stripe.key
 
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Custom Widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+
+	data := make(map[string]any)
+	data["widget"] = widget
+
 	if err := app.renderTemplate(w, r, "buy-once", &templateData{
 		StringMap: stringMap,
+		Data:      data,
 	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
