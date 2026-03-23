e14cdff04ffd9b270f15d93cdfa36d7145826895
Max Kranker <maxkranker@protonmail.com>
2026-03-23 18:46:28 -0400

    Initialize first soda migration

---
+++ migrations/schema.sql
@@ -0,0 +1,57 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict IBASHxA2p44ghZJow18a4tadC0HUaNEFQHGLzMRJYyzj38QeAHuclhjYC2PaKy8
+
+-- Dumped from database version 18.1 (Debian 18.1-1.pgdg13+2)
+-- Dumped by pg_dump version 18.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: schema_migration; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.schema_migration (
+    version character varying(14) NOT NULL
+);
+
+
+ALTER TABLE public.schema_migration OWNER TO postgres;
+
+--
+-- Name: schema_migration schema_migration_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.schema_migration
+    ADD CONSTRAINT schema_migration_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: schema_migration_version_idx; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USING btree (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict IBASHxA2p44ghZJow18a4tadC0HUaNEFQHGLzMRJYyzj38QeAHuclhjYC2PaKy8
+
