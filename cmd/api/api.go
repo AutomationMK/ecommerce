546c9d67fcbc839c0c6c060fe3c1becdf627926e
Max Kranker <maxkranker@protonmail.com>
2026-03-23 18:07:28 -0400

    Add database connection to application struct

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/AutomationMK/ecommerce/internal/driver"
+	"github.com/AutomationMK/ecommerce/internal/models"
 )
 
 const version = "0.1.0"
@@ -31,6 +32,7 @@ type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
 	version  string
+	DB       models.DBModel
 }
 
 func (app *application) serve() error {
@@ -78,6 +80,7 @@ func main() {
 		infoLog:  infoLog,
 		errorLog: errorLog,
 		version:  version,
+		DB:       models.DBModel{DB: conn},
 	}
 
 	err = app.serve()
