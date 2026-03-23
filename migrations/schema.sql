9873005699923562b8279888c944228cc29fbc07
Max Kranker <maxkranker@protonmail.com>
2026-03-23 19:52:30 -0400

    Migrate to create widgets and transaction_statuses tables

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict IBASHxA2p44ghZJow18a4tadC0HUaNEFQHGLzMRJYyzj38QeAHuclhjYC2PaKy8
+\restrict 06YBboyADPIsWmqc0CGtccKzXGmAXzfUE6UTZsONmeRQeL7vUk7hpIJcMpUhv2U
 
 -- Dumped from database version 18.1 (Debian 18.1-1.pgdg13+2)
 -- Dumped by pg_dump version 18.3
@@ -35,11 +35,116 @@ CREATE TABLE public.schema_migration (
 ALTER TABLE public.schema_migration OWNER TO postgres;
 
 --
+-- Name: transaction_statuses; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.transaction_statuses (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    updated_at timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.transaction_statuses OWNER TO postgres;
+
+--
+-- Name: transaction_statuses_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.transaction_statuses_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.transaction_statuses_id_seq OWNER TO postgres;
+
+--
+-- Name: transaction_statuses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.transaction_statuses_id_seq OWNED BY public.transaction_statuses.id;
+
+
+--
+-- Name: widgets; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.widgets (
+    id integer NOT NULL,
+    name character varying(255) DEFAULT ''::character varying NOT NULL,
+    description text DEFAULT ''::text NOT NULL,
+    inventory_level integer NOT NULL,
+    price integer NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    updated_at timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.widgets OWNER TO postgres;
+
+--
+-- Name: widgets_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.widgets_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.widgets_id_seq OWNER TO postgres;
+
+--
+-- Name: widgets_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.widgets_id_seq OWNED BY public.widgets.id;
+
+
+--
+-- Name: transaction_statuses id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.transaction_statuses ALTER COLUMN id SET DEFAULT nextval('public.transaction_statuses_id_seq'::regclass);
+
+
+--
+-- Name: widgets id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.widgets ALTER COLUMN id SET DEFAULT nextval('public.widgets_id_seq'::regclass);
+
+
+--
 -- Name: schema_migration schema_migration_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.schema_migration
     ADD CONSTRAINT schema_migration_pkey PRIMARY KEY (version);
+
+
+--
+-- Name: transaction_statuses transaction_statuses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.transaction_statuses
+    ADD CONSTRAINT transaction_statuses_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: widgets widgets_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.widgets
+    ADD CONSTRAINT widgets_pkey PRIMARY KEY (id);
 
 
 --
@@ -53,5 +158,5 @@ CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USIN
 -- PostgreSQL database dump complete
 --
 
-\unrestrict IBASHxA2p44ghZJow18a4tadC0HUaNEFQHGLzMRJYyzj38QeAHuclhjYC2PaKy8
+\unrestrict 06YBboyADPIsWmqc0CGtccKzXGmAXzfUE6UTZsONmeRQeL7vUk7hpIJcMpUhv2U
 
