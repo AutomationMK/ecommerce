6dbd758eb8b169af078fa41ccb7014e1e464c565
Max Kranker <maxkranker@protonmail.com>
2026-03-23 17:51:34 -0400

    Add database connection to application struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/AutomationMK/ecommerce/internal/driver"
+	"github.com/AutomationMK/ecommerce/internal/models"
 )
 
 const version = "1.0.0"
@@ -35,6 +36,7 @@ type application struct {
 	errorLog      *log.Logger
 	templateCache map[string]*template.Template
 	version       string
+	DB            models.DBModel
 }
 
 func (app *application) serve() error {
@@ -86,6 +88,7 @@ func main() {
 		errorLog:      errorLog,
 		templateCache: tc,
 		version:       version,
+		DB:            models.DBModel{DB: conn},
 	}
 
 	err = app.serve()
