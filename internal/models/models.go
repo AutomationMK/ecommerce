5ea8b1c026fec9d988ad1496620a007aad089143
Max Kranker <maxkranker@protonmail.com>
2026-03-23 21:21:32 -0400

    Add Users type

--- internal/models/models.go
+++ internal/models/models.go
@@ -96,3 +96,14 @@ type Transaction struct {
 	CreatedAt           time.Time `json:"-"`
 	UpdatedAt           time.Time `json:"-"`
 }
+
+// User is the type for all users
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
