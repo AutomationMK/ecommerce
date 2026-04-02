529388b2103bc9126fb01742946a0d41d5d7baaf
Max Kranker <maxkranker@protonmail.com>
2026-04-02 19:04:42 -0400

    Add SubscribeToPlan Card method

--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -2,6 +2,7 @@ package cards
 
 import (
 	"github.com/stripe/stripe-go/customer"
+	"github.com/stripe/stripe-go/sub"
 	"github.com/stripe/stripe-go/v84"
 	"github.com/stripe/stripe-go/v84/paymentintent"
 	"github.com/stripe/stripe-go/v84/paymentmethod"
@@ -73,6 +74,29 @@ func (c *Card) RetrievePaymentIntent(id string) (*stripe.PaymentIntent, error) {
 	}
 
 	return pi, nil
+}
+
+// SubscribeToPlan creates a subscription ID based on subcription details
+func (c *Card) SubscribeToPlan(cust *stripe.Customer, plan, email, last4, cardType string) (string, error) {
+	stripeCustomerID := cust.ID
+	items := []*stripe.SubscriptionItemsParams{
+		{Plan: stripe.String(plan)},
+	}
+
+	params := &stripe.SubscriptionParams{
+		Customer: stripe.String(stripeCustomerID),
+		Items:    items,
+	}
+
+	params.AddMetadata("last_four", last4)
+	params.AddMetadata("card_type", cardType)
+	params.AddExpand("latest_invoice.payment_intent")
+	subscription, err := sub.New(params)
+	if err != nil {
+		return "", err
+	}
+
+	return subscription.ID, nil
 }
 
 // CreateCustomer creates a stripe customer to use for subscriptions
