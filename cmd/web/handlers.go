d66137f0cfe39b9967d19ad9cef85870bdec7821
Max Kranker <maxkranker@protonmail.com>
2026-05-23 12:32:52 -0400

    Add LoginPage Handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -312,3 +312,10 @@ func (app *application) BronzePlanReceipt(w http.ResponseWriter, r *http.Request
 		app.errorLog.Println(err)
 	}
 }
+
+// LoginPage displays a login page
+func (app *application) LoginPage(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "login", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
